a4dcfc44eaf570400a923a667ecac9a40b306c72
Subanesh-swe <subaneshtechie@gmail.com>
2023-08-02 09:46:10 +0530

    1672. Richest Customer Wealth

---
+++ 2023_08_02/1672. Richest Customer Wealth.py
@@ -0,0 +1,7 @@
+# 1672. Richest Customer Wealth
+
+# https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(sum(row) for row in accounts)
